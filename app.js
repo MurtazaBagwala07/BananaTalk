7c84485ea54224273e9ed5c74e1ce72bf88b0fb3
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-04-13 16:22:55 +0530

    taken input

---
+++ app.js
@@ -0,0 +1,8 @@
+var btnTranslate = document.querySelector("#btn-Translate");
+var txtInput = document.querySelector('#txt-input');
+console.log(txtInput)
+function clickHandler(){
+    console.log("Clicked");
+    console.log("input",txtInput.value);
+};
+btnTranslate.addEventListener("click", clickHandler)
