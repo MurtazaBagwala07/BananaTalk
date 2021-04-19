d1ed05d7b572d1ccc121afd2308ff102f748d877
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-04-20 00:08:28 +0530

    removed comments

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate = document.querySelector("#btn-Translate");
 var txtInput = document.querySelector('#txt-input');
 var outputDiv = document.querySelector('#output');
 
-//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
@@ -16,7 +16,7 @@ function errorHandler(error){
 }
 
 function clickHandler(){
-    ///outputDiv.innerText = "MiniMoona " + txtInput.value;
+    
     var inputText = txtInput.value;
     fetch(getTranslationURL(inputText))
     .then(response => response.json())
